e2fa6293b41f95bef492c48b48e5f36801e0ebc3
FilipTheGod <filipstan666@gmail.com>
2025-04-11 16:00:41 -0400

    mobile view +logo 3

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -12,81 +12,84 @@ const Header: React.FC = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <header className="sticky top-0 z-50 py-4 px-6 flex justify-between items-center bg-laboratory-white">
-      <Link href="/" className="flex items-center">
-        {/* Replace text logo with image logo */}
-        <Image
-          src="/images/GregLab_Logo2"
-          alt="GREG LABORATORY"
-          width={150}
-          height={30}
-          className="object-contain"
-        />
-      </Link>
-
-      {/* Mobile menu button */}
-      <button
-        onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
-        className="md:hidden text-laboratory-black"
-      >
-        {mobileMenuOpen ? 'CLOSE' : 'MENU'}
-      </button>
-
-      {/* Desktop Navigation */}
-      <div className="hidden md:flex items-center space-x-8">
-        <Link
-          href="https://www.instagram.com/greglaboratory/?hl=en"
-          className="text-laboratory-black hover:opacity-70 transition text-xs tracking-wide"
-        >
-          INSTAGRAM
-        </Link>
-        <Link
-          href="/consultancy"
-          className="text-laboratory-black text-xs tracking-wide hover:opacity-70 transition"
-        >
-          CONSULTANCY DIVISION
+    <>
+      <header className="sticky top-0 z-50 py-4 px-6 flex justify-between items-center">
+        <Link href="/" className="flex items-center">
+          {/* Use a consistent format for width/height as numbers */}
+          <Image
+            src="/images/Logo1.png"
+            alt="GREG LABORATORY"
+            width={150}
+            height={30}
+            className="object-contain"
+          />
         </Link>
 
+        {/* Mobile menu button */}
         <button
-          onClick={toggleCart}
-          className="text-laboratory-black text-xs tracking-wide hover:opacity-70 transition"
+          onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
+          className="md:hidden text-laboratory-black text-xs tracking-wide"
         >
-          CART ({cartCount})
+          {mobileMenuOpen ? "CLOSE" : "MENU"}
         </button>
-      </div>
 
-      {/* Mobile Navigation */}
-      {mobileMenuOpen && (
-        <div className="absolute top-full left-0 right-0 bg-laboratory-white p-4 flex flex-col space-y-4 md:hidden">
+        {/* Desktop Navigation */}
+        <div className="hidden md:flex items-center space-x-8">
           <Link
             href="https://www.instagram.com/greglaboratory/?hl=en"
             className="text-laboratory-black hover:opacity-70 transition text-xs tracking-wide"
-            onClick={() => setMobileMenuOpen(false)}
           >
             INSTAGRAM
           </Link>
           <Link
             href="/consultancy"
             className="text-laboratory-black text-xs tracking-wide hover:opacity-70 transition"
-            onClick={() => setMobileMenuOpen(false)}
           >
             CONSULTANCY DIVISION
           </Link>
 
           <button
-            onClick={() => {
-              toggleCart();
-              setMobileMenuOpen(false);
-            }}
-            className="text-laboratory-black text-xs tracking-wide hover:opacity-70 transition text-left"
+            onClick={toggleCart}
+            className="text-laboratory-black text-xs tracking-wide hover:opacity-70 transition"
           >
             CART ({cartCount})
           </button>
         </div>
-      )}
 
-      <Cart />
-    </header>
+        <Cart />
+      </header>
+
+      {/* Mobile Navigation - Fixed at bottom */}
+      {mobileMenuOpen && (
+        <div className="fixed bottom-0 left-0 right-0  p-4 z-40 md:hidden">
+          <div className="flex justify-between items-center">
+            <Link
+              href="https://www.instagram.com/greglaboratory/?hl=en"
+              className="text-laboratory-black hover:opacity-70 transition text-xs tracking-wide"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              INSTAGRAM
+            </Link>
+            <Link
+              href="/consultancy"
+              className="text-laboratory-black text-xs tracking-wide hover:opacity-70 transition"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              CONSULTANCY
+            </Link>
+            <button
+              onClick={() => {
+                toggleCart()
+                setMobileMenuOpen(false)
+              }}
+              className="text-laboratory-black text-xs tracking-wide hover:opacity-70 transition"
+            >
+              CART ({cartCount})
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 
